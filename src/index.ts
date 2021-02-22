452bb964d25ab39da76d2ce054301d71eac141a4
Ross MacArthur <ross@macarthur.io>
2021-02-22 10:58:41 +0200

    Also install `mdbook-linkcheck`

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,20 @@
 import * as core from "@actions/core";
 import * as setup from "@extractions/setup-crate";
 
+async function install(spec: setup.Tool) {
+  const tool = await setup.checkOrInstallTool(spec);
+  core.addPath(tool.dir);
+  core.info(`Successfully setup ${tool.name} v${tool.version}`);
+}
+
 async function main() {
   try {
     const versionSpec = core.getInput("mdbook-version");
-    const tool = await setup.checkOrInstallTool({
-      owner: "rust-lang",
-      name: "mdBook",
-      versionSpec,
+    await install({ owner: "rust-lang", name: "mdBook", versionSpec });
+    await install({
+      owner: "Michael-F-Bryan",
+      name: "mdbook-linkcheck",
     });
-    core.addPath(tool.dir);
-    core.info(`Successfully setup ${tool.name} v${tool.version}`);
   } catch (err) {
     core.setFailed(err.message);
   }
